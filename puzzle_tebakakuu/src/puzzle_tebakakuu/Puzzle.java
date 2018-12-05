8bc4817f2a0a722e5c42bc6d2e8b168de3fed766
rpersada <43272995+rpersada@users.noreply.github.com>
2018-12-06 01:57:20 +0700

    Update Puzzle.java

--- puzzle_tebakakuu/src/puzzle_tebakakuu/Puzzle.java
+++ puzzle_tebakakuu/src/puzzle_tebakakuu/Puzzle.java
@@ -3,62 +3,78 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package puzzle_tebakakuu;
+package tebk;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
-import static puzzle_tebakakuu.main.frame;
-import static puzzle_tebakakuu.main.puzzle;
+import static tebk.main.frame;
+import static tebk.main.puzzle;
+import java.awt.CardLayout;
 /**
  *
  * @author persada
  */
 public class Puzzle extends JPanel{
-    Segment[] segments;
+        Segment[] segments;
+        public  static String info;
 	Image img;
-        CardLayout cl;
+        //int score =0;
+       
+       
         
         
        
         
-	
+
+        
 	public boolean started = false;
 	public boolean mixing = false;
-    
+        
         public Puzzle(Image img) {
 		this.img = img;
-                
-		//init 9 segments
+		
 		segments = new Segment[9];
 		int segmentSize = img.getWidth(null)/3;
 		for (int i = 0; i != segments.length; i++) {
-			//Pass the Segment the instance of the Game, its position (by the index) and its size
+			
 			segments[i] = new Segment(this, i, segmentSize);
 		}
+                start();
                 addMouseListener(new MouseAdapter() {
                         @Override
 			public void mousePressed(MouseEvent e) {
-                            if (!started) 
-                                start();
-                            else if (!mixing) 
+//                            if (!started) 
+//                                start();
+                            if (!mixing) 
                                 onClick(e);
                             else 
                                 mixing = false;
 			}
 		});
-	}
+                 
+        
+        
+       
+        
+        
+        }
         public void start() {
 		started = true;
-		//remove segment 8 (the one in the bottom right)
+		
 		segments[8].isEmpty = true;
 		mix.start();
 	}
-        //This Thread shuffles the Segments by switching one of the neighboring segments of the empty one with the empty one
-	Thread mix = new Thread(new Runnable() {
+        Thread mix = new Thread(new Runnable() {
 		public void run() {
 			mixing = true;
-			while (mixing) {
+			                 for (int i = 0; i < 50; i++) {
+                        
+                    
 				ArrayList<Integer> possibleMovements = new ArrayList<Integer>();
 				for (Segment s : segments) {
 					if (s.getPosition().x == segments[8].getPosition().x+1 && s.getPosition().y == segments[8].getPosition().y) possibleMovements.add(s.getID());
@@ -75,21 +91,27 @@ public class Puzzle extends JPanel{
 				} catch (Exception e) {}
 				repaint();
 				try {
-					Thread.sleep(100);
+					Thread.sleep(50);
 				} catch (InterruptedException e) {}
 			}
 		}
 	});
         
-        //Check if the user clicked onto a seg,emt amd of possible move it onto the empty one
-	public void onClick(MouseEvent e) {
+//    public void setrunTimer(int detikAtas){
+//        this.detikAtas = detikAtas;        
+//    }   
+//    public int getrunTimer(){
+//        return this.detikAtas;
+//    }
+        
+        public void onClick(MouseEvent e) {
 		for (Segment s : segments) {
 			if (s.hitten(e.getPoint())) {
 				Point tmp = s.getPosition();
 				if (s.move(segments[8].getPosition())) {
 					segments[8].setPosition(tmp);
 					
-					//Check if user is done by comparing it's position with the one it should have
+			
 					boolean done = true;
 					for (int i = 0; i != 9; i++) {
 						if (segments[i].getPosition().x == ((i <= 2)? i:(i <= 5)? (i-3):(i-6)) && segments[i].getPosition().y == (int) Math.ceil((i/3))) {
@@ -97,18 +119,33 @@ public class Puzzle extends JPanel{
 						} else {
 							//System.out.println(i+": :(");
 							done = false;
-                                                        
+                                                       
+                                                          
 						}
-                                                
 					}
 					
-					if   (done) {
+					if (done) {
+                                                
 						started = false;
 						segments[8].isEmpty = false;
-                                                informasi1 i1 = new informasi1 ();
-                                                i1.setVisible(true);
-                                                this.dispose();
+                                                System.out.println("selesai");
+                                                
+                                                
+                                                
+                                               
+                                                //JOptionPane.showMessageDialog(null, "Berhasil:");
+//                                                JOptionPane.showMessageDialog(null, "Skor:"+ score);
+                                                new info().setVisible(true);
+                                               
 					}
+                                        
+                                    
+                                        
+                                    
+                                        
+                                    
+                                        
+                                    
 				}
 			}
 		}
@@ -122,14 +159,6 @@ public class Puzzle extends JPanel{
 			segments[i].paint(g);
 		}
 	}
-        
-        
-
-    private void dispose() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    
-    
+	
 }
+	
